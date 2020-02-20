5c605b15cf311670fb3132573929f4302bf86cd1
William Goode <wgoode3@gmail.com>
2020-02-20 13:26:39 -0500

    initial

---
+++ more-algos.js
@@ -0,0 +1,43 @@
+/*
+ * Evens Out
+ * given an array of numbers find the sum of all the even values
+ * [3,6,1,12,-9,-4] should return 14
+ */
+
+/*
+ * Count Adjacent
+ * given an array of 0s and 1s count how many times 
+ * there are consecutive 0s and consecutive 1s
+ * [0, 0, 1, 1, 0] should return 2
+ * [0, 1, 0, 1, 0] should return 0
+ */
+
+/*
+ * Even Steps (aka lerp)
+ * given a "start", "end", and "size" construct an
+ * array that has "size" number of values starting at "start"
+ * and ending at "end" with even increments between each value
+ * range(-1,7,4) should return [-1, 1.667, 4.333, 7]
+ * range(10,2,5) should return [10, 8, 6, 4, 2]
+ */
+
+/*
+ * Sorted Greater than Y
+ * Given an array of numbers (in order) and a value "y"
+ * return a count of how many values are greater than "y"
+ * [4,7,18,49,77,102,587,2e6], 50 should return 4
+ */
+
+/*
+ * High Pass Filter
+ * Given an array of values "arr" and a number "filter" return an
+ * array with only the numbers greater than "filter"
+ * [-1,3,6,7,6,3,-1,3,6], 4 should return [6,7,6,6]
+ */
+
+/*
+ * Band Pass Filter
+ * Given an array of values "arr", a "high" number and a "low" number
+ * return an array with only values between this range (inclusive)
+ * [-1,3,6,7,6,3,-1,3,6], 2,6 should return [3,6,6,3,3]
+ */
